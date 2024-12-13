e9ec405e60cd2d1a52dffc908a9d086a5d8be240
francoleon42 <francoleonadress@gmail.com>
2024-12-13 20:46:02 -0300

    Create HistorialUsuarioController.java

---
+++ src/main/java/com/teset/controller/HistorialUsuarioController.java
@@ -0,0 +1,15 @@
+package com.teset.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/historial")
+@RequiredArgsConstructor
+public class HistorialUsuarioController {
+
+    
+}
